80e23df50242b0088cfc3636764e3df97256f968
KP18 Gamer <74746579+diwasatreya@users.noreply.github.com>
2021-08-16 13:52:32 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    Version: require("./package.json").version,
+    Hug: require("./gif/hug.js").Hug,
+    Bite: require("./gif/bite.js").Bite,
+    Blush: require("./gif/blush.js").Blush,
+    Cry: require("./gif/cry.js").Cry,
+    Cuddle: require("./gif/cuddle.js").Cuddle,
+    Dance: require("./gif/dance.js").Dance,
+    Eevee: require("./gif/eevee.js").Eevee,
+    Feed: require("./gif/feed.js").Feed,
+    Fluff: require("./gif/fluff.js").Fluff,
+    Kiss: require("./gif/kiss.js").Kiss,
+    Lick: require("./gif/lick.js").Lick,
+    Neko: require("./gif/neko.js").Neko,
+    Pat: require("./gif/pat.js").Pat,
+    Poke: require("./gif/poke.js").Poke,
+    Slap: require("./gif/slap.js").Slap,
+    Smile: require("./gif/smile.js").Smile
+  
+};
